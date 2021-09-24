15e6d6536defbbe14edd09de037228a51e9ed8a5
James Page <james.page@ubuntu.com>
2021-09-24 09:22:42 +0100

    Drop volume service, will be provided by backend operators

--- charms/cinder-k8s/src/charm.py
+++ charms/cinder-k8s/src/charm.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-# Copyright 2021 James Page
-# See LICENSE file for licensing details.
-#
-# Learn more at: https://juju.is/docs/sdk
+"""Cinder Operator Charm.
 
-"""Charm the service.
-
-Refer to the following post for a quick-start guide that will help you
-develop a new k8s charm using the Operator Framework:
-
-    https://discourse.charmhub.io/t/4208
+This charm provide Cinder services as part of an OpenStack deployment
 """
 
 import logging
+
+from charms.nginx_ingress_integrator.v0.ingress import IngressRequires
+from charms.mysql.v1.mysql import MySQLConsumer
 
 from ops.charm import CharmBase
 from ops.framework import StoredState
@@ -21,84 +16,114 @@ from ops.model import ActiveStatus
 
 logger = logging.getLogger(__name__)
 
+CINDER_API_PORT = 8090
 
-class CharmCinderOperatorCharm(CharmBase):
+
+class CinderOperatorCharm(CharmBase):
     """Charm the service."""
 
     _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
 
-    def _on_httpbin_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
+        self.framework.observe(self.on.cinder_api_pebble_ready,
+                               self._on_cinder_api_pebble_ready)
+        self.framework.observe(self.on.cinder_scheduler_pebble_ready,
+                               self._on_cinder_scheduler_pebble_ready)
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        You'll need to specify the right entrypoint and environment
-        configuration for your specific workload. Tip: you can see the
-        standard entrypoint of an existing container using docker inspect
+        self.framework.observe(self.on.config_changed,
+                               self._on_config_changed)
 
-        Learn more about Pebble layers at https://github.com/canonical/pebble
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+        # Register the database consumer and register for events
+        self.db = MySQLConsumer(self, 'cinder-db', {"mysql": ">=8"})
+        self.framework.observe(self.on.cinder_db_relation_changed,
+                               self._on_database_changed)
+
+        # Access to API service from outside of K8S
+        self.ingress_public = IngressRequires(self, {
+            'service-hostname': self.model.config['os-public-hostname'],
+            'service-name': self.app.name,
+            'service-port': CINDER_API_PORT,
+        })
+
+        self._stored.set_default(db_ready=False)
+        self._stored.set_default(amqp_ready=False)
+        self._stored.set_default(identity_ready=False)
+
+        # TODO
+        # Register AMQP consumer + events
+        # Register Identity Service consumer + events
+
+        # TODO
+        # State modelling
+        # DB & AMQP & Identity -> API and Scheduler
+        # Store URL's etc on _changed events?
+
+    @property
+    def _pebble_cinder_api_layer(self):
+        """Pebble layer for Cinder API"""
+        return {
+            "summary": "cinder layer",
+            "description": "pebble configuration for cinder services",
             "services": {
-                "httpbin": {
+                "cinder-api": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
+                    "summary": "Cinder API",
+                    "command": "/usr/sbin/apache2ctl -DFOREGROUND",
+                    "startup": "enabled"
                 }
-            },
+            }
         }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
+
+    @property
+    def _pebble_cinder_scheduler_layer(self):
+        """Pebble layer for Cinder Scheduler"""
+        return {
+            "summary": "cinder layer",
+            "description": "pebble configuration for cinder services",
+            "services": {
+                "cinder-scheduler": {
+                    "override": "replace",
+                    "summary": "Cinder Scheduler",
+                    "command": "cinder-scheduler --use-syslog",
+                    "startup": "enabled"
+                }
+            }
+        }
+
+    def _on_cinder_api_pebble_ready(self, event):
+        """Define and start a workload using the Pebble API."""
+        container = event.workload
+        container.add_layer("cinder-api", self._pebble_cinder_api_layer, combine=True)
         container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ActiveStatus()
+
+    def _on_cinder_scheduler_pebble_ready(self, event):
+        """Define and start a workload using the Pebble API."""
+        container = event.workload
+        container.add_layer("cinder-scheduler", self._pebble_cinder_scheduler_layer, combine=True)
+        container.autostart()
 
     def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
+        """Just an example to show how to deal with changed configuration"""
+        # TODO
+        # Set debug logging and restart services
+        pass
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
+    def _on_database_ready(self, event):
+        """Database ready for use"""
+        # TODO
+        # Run sync process if on lead unit
+        pass
 
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+    def _on_amqp_ready(self, event):
+        """AMQP service ready for use"""
+        pass
 
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
-
-        Learn more about actions at https://juju.is/docs/sdk/actions
-        """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
-        else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+    def _on_identity_service_ready(self, event):
+        """Identity service ready for use"""
+        pass
 
 
 if __name__ == "__main__":
-    main(CharmCinderOperatorCharm)
+    main(CinderOperatorCharm)
