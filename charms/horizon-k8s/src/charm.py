97fdda709585bdd74c6e55b6f142bb97b5d87df3
Liam Young <liam.young@canonical.com>
2022-05-10 11:02:14 +0100

    Initial cut

---
+++ charms/horizon-k8s/src/charm.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+"""OpenstackDashboard Operator Charm.
+
+This charm provide OpenstackDashboard services as part of an OpenStack
+deployment
+"""
+
+import logging
+from typing import List
+
+import ops.framework
+from ops.main import main
+
+import advanced_sunbeam_openstack.core as sunbeam_core
+import advanced_sunbeam_openstack.charm as sunbeam_charm
+import advanced_sunbeam_openstack.container_handlers as sunbeam_chandlers
+
+logger = logging.getLogger(__name__)
+
+
+class WSGIDashboardPebbleHandler(sunbeam_chandlers.WSGIPebbleHandler):
+
+    def init_service(self, context: sunbeam_core.OPSCharmContexts) -> None:
+        """Enable and start WSGI service."""
+        container = self.charm.unit.get_container(self.container_name)
+        try:
+            process = container.exec(
+                ['a2dissite', '000-default'],
+                timeout=5*60)
+            out, warnings = process.wait_output()
+            if warnings:
+                for line in warnings.splitlines():
+                    logger.warning('a2dissite warn: %s', line.strip())
+            logging.debug(f'Output from a2dissite: \n{out}')
+        except ops.pebble.ExecError:
+            logger.exception(
+                f"Failed to disable default site in apache"
+            )
+        super().init_service(context)
+
+
+class OpenstackDashboardOperatorCharm(sunbeam_charm.OSBaseOperatorAPICharm):
+    """Charm the service."""
+
+    _state = ops.framework.StoredState()
+    service_name = "openstack-dashboard"
+    wsgi_admin_script = (
+        "/usr/share/openstack-dashboard/openstack_dashboard/wsgi/django.wsgi")
+    wsgi_public_script = (
+        "/usr/share/openstack-dashboard/openstack_dashboard/wsgi/django.wsgi")
+
+    db_sync_cmds = [
+        [
+            'python3',
+            '/usr/share/openstack-dashboard/manage.py',
+            'migrate',
+            '--noinput']
+    ]
+
+    @property
+    def default_public_ingress_port(self):
+        return 80
+
+    @property
+    def apache_vhost(self) -> str:
+        """Service default configuration file."""
+        return f"/etc/apache2/sites-enabled/openstack-dashboard.conf"
+
+    @property
+    def service_conf(self) -> str:
+        """Service default configuration file."""
+        return f"/etc/openstack-dashboard/local_settings.py"
+
+    @property
+    def service_user(self) -> str:
+        """Service user file and directory ownership."""
+        return 'horizon'
+
+    @property
+    def service_group(self) -> str:
+        """Service group file and directory ownership."""
+        return 'horizon'
+
+    @property
+    def service_endpoints(self):
+        return [
+            {
+                'service_name': 'openstack-dashboard',
+                'type': 'openstack-dashboard',
+                'description': "OpenStack OpenstackDashboard API",
+                'internal_url': f'{self.internal_url}',
+                'public_url': f'{self.public_url}',
+                'admin_url': f'{self.admin_url}'}]
+
+    def get_pebble_handlers(self) -> List[sunbeam_chandlers.PebbleHandler]:
+        """Pebble handlers for the service."""
+        return [
+            WSGIDashboardPebbleHandler(
+                self,
+                self.service_name,
+                self.service_name,
+                self.container_configs,
+                self.template_dir,
+                self.openstack_release,
+                self.configure_charm,
+                f"wsgi-{self.service_name}",
+            )
+        ]
+
+    def configure_charm(self, event: ops.framework.EventBase) -> None:
+        """Configure charm services."""
+        super().configure_charm(event)
+        if self.bootstrapped():
+            self.unit.status = ops.model.ActiveStatus(self.ingress.url)
+
+
+class OpenstackDashboardWallabyOperatorCharm(OpenstackDashboardOperatorCharm):
+
+    openstack_release = 'wallaby'
+
+if __name__ == "__main__":
+    # Note: use_juju_for_storage=True required per
+    # https://github.com/canonical/operator/issues/506
+    main(OpenstackDashboardWallabyOperatorCharm, use_juju_for_storage=True)
---
+++ charms/horizon-k8s/unit_tests/test_dashboard_charm.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+import sys
+
+sys.path.append('lib')  # noqa
+sys.path.append('src')  # noqa
+
+import charm
+import advanced_sunbeam_openstack.test_utils as test_utils
+
+
+class _DashboardWallabyOperatorCharm(
+        charm.OpenstackDashboardWallabyOperatorCharm):
+
+    def __init__(self, framework):
+        self.seen_events = []
+        super().__init__(framework)
+
+    def _log_event(self, event):
+        self.seen_events.append(type(event).__name__)
+
+    def configure_charm(self, event):
+        super().configure_charm(event)
+        self._log_event(event)
+
+    @property
+    def public_ingress_address(self):
+        return 'dashboard.juju'
+
+
+class TestDashboardOperatorCharm(test_utils.CharmTestCase):
+
+    PATCHES = []
+
+    @mock.patch(
+        'charms.observability_libs.v0.kubernetes_service_patch.'
+        'KubernetesServicePatch')
+    def setUp(self, mock_patch):
+        super().setUp(charm, self.PATCHES)
+        self.harness = test_utils.get_harness(
+            _DashboardWallabyOperatorCharm,
+            container_calls=self.container_calls)
+        self.addCleanup(self.harness.cleanup)
+        test_utils.add_complete_ingress_relation(self.harness)
+        self.harness.begin()
+
+    def test_pebble_ready_handler(self):
+        self.assertEqual(self.harness.charm.seen_events, [])
+        test_utils.set_all_pebbles_ready(self.harness)
+        self.assertEqual(self.harness.charm.seen_events, ['PebbleReadyEvent'])
+
+    def test_all_relations(self):
+        self.harness.set_leader()
+        test_utils.set_all_pebbles_ready(self.harness)
+        test_utils.add_all_relations(self.harness)
+        setup_cmds = [
+            ['a2dissite', '000-default'],
+            ['a2ensite', 'wsgi-openstack-dashboard'],
+            ['python3', '/usr/share/openstack-dashboard/manage.py', 'migrate',
+             '--noinput']]
+        for cmd in setup_cmds:
+            self.assertIn(
+                cmd,
+                self.container_calls.execute['openstack-dashboard'])
+        self.check_file(
+            'openstack-dashboard',
+            '/etc/apache2/sites-available/wsgi-openstack-dashboard.conf')
+        self.check_file(
+            'openstack-dashboard',
+            '/etc/openstack-dashboard/local_settings.py')
