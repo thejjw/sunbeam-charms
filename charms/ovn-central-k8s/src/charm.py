98d56d00736b73170bc1f21e1d64a798e1138877
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2022-07-26 15:38:04 +0530

    Sync global tox, requirement files
    Following changes are done as part of this patch:
    * Sync global tox, requirements.txt, test-requirements
    from release-tools project
    * Resolve flake errors
    * Fix templates to handle optional ingress relations
    * Remove run_tests script
    * Restructure unit tests directory

--- charms/ovn-central-k8s/src/charm.py
+++ charms/ovn-central-k8s/src/charm.py
@@ -367,6 +367,7 @@ class OVNCentralXenaOperatorCharm(OVNCentralOperatorCharm):
 
     openstack_release = 'xena'
 
+
 if __name__ == "__main__":
     # Note: use_juju_for_storage=True required per
     # https://github.com/canonical/operator/issues/506
