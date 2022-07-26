0a1d60d41e41ebbd9b8e3bc6deed6c49a3d3a82f
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2022-07-26 15:39:55 +0530

    Sync global tox, requirement files
    Following changes are done as part of this patch:
    * Sync global tox, requirements.txt, test-requirements
    from release-tools project
    * Resolve flake errors
    * Restructure unit tests directory

--- charms/cinder-ceph-k8s/tests/unit/test_cinder_ceph_charm.py
+++ charms/cinder-ceph-k8s/tests/unit/test_cinder_ceph_charm.py
@@ -14,11 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import json
-
-sys.path.append("lib")  # noqa
-sys.path.append("src")  # noqa
 
 import charm
 import ops_sunbeam.test_utils as test_utils
