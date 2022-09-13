f466346c70a82777bbb8cea93ef687b493d4e9a7
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2022-09-13 18:05:26 +0530

    Update traefik ingress library to v1
    Fetch traefik ingress library v1 and remove v0.
    
    Depends-On: https://review.opendev.org/c/openstack/charm-ops-sunbeam/+/856982
    
    Change-Id: I746dc1d145d0dc52bbe2d028c5e583ae61e091f4

--- charms/cinder-k8s/fetch-libs.sh
+++ charms/cinder-k8s/fetch-libs.sh
@@ -6,4 +6,4 @@ charmcraft fetch-lib charms.data_platform_libs.v0.database_requires
 charmcraft fetch-lib charms.sunbeam_keystone_operator.v0.identity_service
 charmcraft fetch-lib charms.sunbeam_rabbitmq_operator.v0.amqp
 charmcraft fetch-lib charms.observability_libs.v0.kubernetes_service_patch
-charmcraft fetch-lib charms.traefik_k8s.v0.ingress
+charmcraft fetch-lib charms.traefik_k8s.v1.ingress
