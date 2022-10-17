19a214e69a778c1fb08dea6b93aec12dc1934f58
James Page <james.page@canonical.com>
2022-10-17 16:33:29 +0100

    Update libs for new namespaces
    Update from old sunbeam POC name to new ovn-central-k8s name.
    
    Change-Id: I743eab39c419ced17fa34bf43439b110d56c4339

--- charms/ovn-relay-k8s/fetch-libs.sh
+++ charms/ovn-relay-k8s/fetch-libs.sh
@@ -2,4 +2,4 @@
 
 echo "INFO: Fetching libs from charmhub."
 charmcraft fetch-lib charms.observability_libs.v0.kubernetes_service_patch
-charmcraft fetch-lib charms.sunbeam_ovn_central_operator.v0.ovsdb
+charmcraft fetch-lib charms.ovn_central_k8s.v0.ovsdb
