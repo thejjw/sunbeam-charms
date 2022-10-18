d44767815f070a03957f78f3b66cba241c17ce37
James Page <james.page@canonical.com>
2022-10-18 09:06:17 +0100

    Update libraries for new locations
    Refresh all interface libraries from new published locations.
    
    Change-Id: Iea22c9dd2e40ca98f5226e04f8dd20115eed6042

--- charms/nova-k8s/fetch-libs.sh
+++ charms/nova-k8s/fetch-libs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "INFO: Fetching libs from charmhub."
-charmcraft fetch-lib charms.nginx_ingress_integrator.v0.ingress
-charmcraft fetch-lib charms.sunbeam_mysql_k8s.v0.mysql
-charmcraft fetch-lib charms.sunbeam_keystone_operator.v0.identity_service
-charmcraft fetch-lib charms.sunbeam_rabbitmq_operator.v0.amqp
+charmcraft fetch-lib charms.data_platform_libs.v0.database_requires
+charmcraft fetch-lib charms.keystone_k8s.v0.identity_service
+charmcraft fetch-lib charms.rabbitmq_k8s.v0.rabbitmq
 charmcraft fetch-lib charms.observability_libs.v0.kubernetes_service_patch
 charmcraft fetch-lib charms.traefik_k8s.v1.ingress
-charmcraft fetch-lib charms.sunbeam_nova_compute_operator.v0.cloud_compute
+charmcraft fetch-lib charms.nova_compute_k8s.v0.cloud_compute
