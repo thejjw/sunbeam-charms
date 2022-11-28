e932c6854cf6526db995829a4cb9b5e02e5dad23
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2022-11-28 16:16:07 +0530

    Use tls-certificates-interface library
    Use tls-certificates-interface library from
    https://charmhub.io/tls-certificates-interface/libraries/tls_certificates
    
    Fix py3 failures due to cryptography package
    clashes from zaza. Move zaza dependcies from
    test-requirements to tox
    
    Depends-On: https://review.opendev.org/c/openstack/charm-ops-sunbeam/+/865410
    Change-Id: I0f211b43bd9a8fec14cccd3699181e7f9bd34542

--- charms/ovn-relay-k8s/fetch-libs.sh
+++ charms/ovn-relay-k8s/fetch-libs.sh
@@ -3,3 +3,4 @@
 echo "INFO: Fetching libs from charmhub."
 charmcraft fetch-lib charms.observability_libs.v0.kubernetes_service_patch
 charmcraft fetch-lib charms.ovn_central_k8s.v0.ovsdb
+charmcraft fetch-lib charms.tls_certificates_interface.v1.tls_certificates
