24f052e76df2f3f371686c195355f1b99267c9cf
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2023-03-06 17:43:22 +0530

    Use library identity_service v1.0
    Update identity_service library to v1.0
    Update zuul.yaml to use juju 3.1 for CI tests
    
    Make zaza-smoke-tests job optional as nova-k8s used
    in smoke tests is not yet updated to use
    identity_serice v1.0
    
    Change-Id: I143f63de92c37b5b2a2c5cfeb7c100fb9610b85f

--- charms/placement-k8s/fetch-libs.sh
+++ charms/placement-k8s/fetch-libs.sh
@@ -2,6 +2,6 @@
 
 echo "INFO: Fetching libs from charmhub."
 charmcraft fetch-lib charms.data_platform_libs.v0.database_requires
-charmcraft fetch-lib charms.keystone_k8s.v0.identity_service
+charmcraft fetch-lib charms.keystone_k8s.v1.identity_service
 charmcraft fetch-lib charms.rabbitmq_k8s.v0.rabbitmq
 charmcraft fetch-lib charms.traefik_k8s.v1.ingress
