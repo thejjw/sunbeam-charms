2ee5a5779a711fe83deba82538762f44a5fc4657
Liam Young <liam.young@canonical.com>
2023-07-13 11:58:23 +0000

    Misc fixes and additional testing
    * Add functional tests. This is currently limited to checking the
      charm deploys and relates to mandatory relations.
    * Add heat-engine container
    * Add management of all 3 containers to charm. Previously only the
      heat-api container was managed and this was incorrectly done on
      the assumption is was a wsgi app
    * Add management for auth_encryption_key
    * Add ops.testing unit tests
    
    Change-Id: I57b24a01ed473c96648f78095dc5e4e87d240e66

--- charms/heat-k8s/src/charm.py
+++ charms/heat-k8s/src/charm.py
@@ -1,17 +1,120 @@
 #!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Heat Operator Charm.
 
-This charm provide Heat services as part of an OpenStack deployment
+This charm provide heat services as part of an OpenStack deployment
 """
 
 import logging
-
-from ops.framework import StoredState
-from ops.main import main
+import secrets
+import string
+from typing import (
+    List,
+)
 
 import ops_sunbeam.charm as sunbeam_charm
+import ops_sunbeam.container_handlers as sunbeam_chandlers
+import ops_sunbeam.core as sunbeam_core
+from ops.framework import (
+    StoredState,
+)
+from ops.main import (
+    main,
+)
 
 logger = logging.getLogger(__name__)
+
+HEAT_API_CONTAINER = "heat-api"
+HEAT_API_CNF_CONTAINER = "heat-api-cfn"
+HEAT_ENGINE_CONTAINER = "heat-engine"
+
+
+class HeatAPIPebbleHandler(sunbeam_chandlers.ServicePebbleHandler):
+    """Pebble handler for Heat API container."""
+
+    def get_layer(self):
+        """Heat API service.
+
+        :returns: pebble service layer configuration for heat api service
+        :rtype: dict
+        """
+        return {
+            "summary": "heat api layer",
+            "description": "pebble configuration for heat api service",
+            "services": {
+                "heat-api": {
+                    "override": "replace",
+                    "summary": "Heat API",
+                    "command": "heat-api",
+                    "startup": "enabled",
+                    "user": "heat",
+                    "group": "heat",
+                }
+            },
+        }
+
+
+class HeatAPICFNPebbleHandler(sunbeam_chandlers.ServicePebbleHandler):
+    """Pebble handler for Heat API CNF container."""
+
+    def get_layer(self):
+        """Heat API CNF service.
+
+        :returns: pebble service layer configuration for API CNF service
+        :rtype: dict
+        """
+        return {
+            "summary": "heat api cfn layer",
+            "description": "pebble configuration for heat api cfn service",
+            "services": {
+                "heat-api-cfn": {
+                    "override": "replace",
+                    "summary": "Heat API CNF",
+                    "command": "heat-api-cfn",
+                    "startup": "enabled",
+                    "user": "heat",
+                    "group": "heat",
+                }
+            },
+        }
+
+
+class HeatEnginePebbleHandler(sunbeam_chandlers.ServicePebbleHandler):
+    """Pebble handler for Heat engine container."""
+
+    def get_layer(self):
+        """Heat Engine service.
+
+        :returns: pebble service layer configuration for heat engine service
+        :rtype: dict
+        """
+        return {
+            "summary": "heat engine layer",
+            "description": "pebble configuration for heat engine service",
+            "services": {
+                "heat-engine": {
+                    "override": "replace",
+                    "summary": "Heat Engine",
+                    "command": "heat-engine",
+                    "startup": "enabled",
+                    "user": "heat",
+                    "group": "heat",
+                }
+            },
+        }
 
 
 class HeatOperatorCharm(sunbeam_charm.OSBaseOperatorAPICharm):
@@ -19,38 +122,104 @@ class HeatOperatorCharm(sunbeam_charm.OSBaseOperatorAPICharm):
 
     _state = StoredState()
     service_name = "heat-api"
-    wsgi_admin_script = '/usr/bin/heat-wsgi-api'
-    wsgi_public_script = '/usr/bin/heat-wsgi-api'
+    wsgi_admin_script = "/usr/bin/heat-wsgi-api"
+    wsgi_public_script = "/usr/bin/heat-wsgi-api"
+    heat_auth_encryption_key = "auth_encryption_key"
 
-    db_sync_cmds = [
-        ['heat-manage', 'db_sync']
-    ]
+    db_sync_cmds = [["heat-manage", "db_sync"]]
+
+    mandatory_relations = {
+        "database",
+        "amqp",
+        "identity-service",
+        "ingress-public",
+    }
+
+    def get_pebble_handlers(
+        self,
+    ) -> List[sunbeam_chandlers.ServicePebbleHandler]:
+        """Pebble handlers for operator."""
+        pebble_handlers = [
+            HeatAPIPebbleHandler(
+                self,
+                HEAT_API_CONTAINER,
+                "heat-api",
+                self.default_container_configs(),
+                self.template_dir,
+                self.configure_charm,
+            ),
+            HeatAPICFNPebbleHandler(
+                self,
+                HEAT_API_CNF_CONTAINER,
+                "heat-api-cfn",
+                self.default_container_configs(),
+                self.template_dir,
+                self.configure_charm,
+            ),
+            HeatEnginePebbleHandler(
+                self,
+                HEAT_ENGINE_CONTAINER,
+                "heat-engine",
+                self.default_container_configs(),
+                self.template_dir,
+                self.configure_charm,
+            ),
+        ]
+        return pebble_handlers
+
+    def get_heat_auth_encryption_key(self):
+        """Return the shared metadata secret."""
+        return self.leader_get(self.heat_auth_encryption_key)
+
+    def set_heat_auth_encryption_key(self):
+        """Store the shared metadata secret."""
+        alphabet = string.ascii_letters + string.digits
+        key = "".join(secrets.choice(alphabet) for i in range(32))
+        self.leader_set({self.heat_auth_encryption_key: key})
+
+    def configure_charm(self, event):
+        """Configure charm.
+
+        Ensure setting the auth key is first as services in container need it
+        to start.
+        """
+        if self.unit.is_leader():
+            auth_key = self.get_heat_auth_encryption_key()
+            if auth_key:
+                logger.debug("Found auth key in leader DB")
+            else:
+                logger.debug("Creating auth key")
+                self.set_heat_auth_encryption_key()
+        super().configure_charm(event)
 
     @property
     def service_conf(self) -> str:
         """Service default configuration file."""
-        return f"/etc/heat/heat.conf"
+        return "/etc/heat/heat.conf"
 
     @property
     def service_user(self) -> str:
         """Service user file and directory ownership."""
-        return 'heat'
+        return "heat"
 
     @property
     def service_group(self) -> str:
         """Service group file and directory ownership."""
-        return 'heat'
+        return "heat"
 
     @property
     def service_endpoints(self):
+        """Return heat service endpoints."""
         return [
             {
-                'service_name': 'heat',
-                'type': 'heat',
-                'description': "OpenStack Heat API",
-                'internal_url': f'{self.internal_url}',
-                'public_url': f'{self.public_url}',
-                'admin_url': f'{self.admin_url}'}]
+                "service_name": "heat",
+                "type": "heat",
+                "description": "OpenStack Heat API",
+                "internal_url": f"{self.internal_url}",
+                "public_url": f"{self.public_url}",
+                "admin_url": f"{self.admin_url}",
+            }
+        ]
 
     def get_healthcheck_layer(self) -> dict:
         """Health check pebble layer.
@@ -68,18 +237,19 @@ class HeatOperatorCharm(sunbeam_charm.OSBaseOperatorAPICharm):
         }
 
     def default_container_configs(self):
-        """Base container configs."""
+        """Return base container configs."""
         return [
             sunbeam_core.ContainerConfigFile(
-                "/etc/heat/heat-api.conf", "heat", "heat"
+                "/etc/heat/heat.conf", "root", "heat"
             ),
             sunbeam_core.ContainerConfigFile(
-                "/etc/heat/api-paste.ini", "heat", "heat"
+                "/etc/heat/api-paste.ini", "root", "heat"
             ),
         ]
 
     @property
     def default_public_ingress_port(self):
+        """Port for Heat AI service."""
         return 8004
 
 
---
+++ charms/heat-k8s/tests/unit/test_heat_charm.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for Heat operator."""
+
+import ops_sunbeam.test_utils as test_utils
+
+import charm
+
+
+class _HeatTestOperatorCharm(charm.HeatOperatorCharm):
+    """Test Operator Charm for Heat Operator."""
+
+    def __init__(self, framework):
+        self.seen_events = []
+        super().__init__(framework)
+
+    def _log_event(self, event):
+        self.seen_events.append(type(event).__name__)
+
+    def configure_charm(self, event):
+        super().configure_charm(event)
+        self._log_event(event)
+
+    @property
+    def public_ingress_address(self):
+        return "heat.juju"
+
+
+class TestHeatOperatorCharm(test_utils.CharmTestCase):
+    """Unit tests for Heat Operator."""
+
+    PATCHES = []
+
+    def setUp(self):
+        """Run setup for unit tests."""
+        super().setUp(charm, self.PATCHES)
+        self.harness = test_utils.get_harness(
+            _HeatTestOperatorCharm, container_calls=self.container_calls
+        )
+
+        # clean up events that were dynamically defined,
+        # otherwise we get issues because they'll be redefined,
+        # which is not allowed.
+        from charms.data_platform_libs.v0.database_requires import (
+            DatabaseEvents,
+        )
+
+        for attr in (
+            "database_database_created",
+            "database_endpoints_changed",
+            "database_read_only_endpoints_changed",
+        ):
+            try:
+                delattr(DatabaseEvents, attr)
+            except AttributeError:
+                pass
+
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
+    def test_pebble_ready_handler(self):
+        """Test pebble ready handler."""
+        self.assertEqual(self.harness.charm.seen_events, [])
+        test_utils.set_all_pebbles_ready(self.harness)
+        self.assertEqual(len(self.harness.charm.seen_events), 3)
+
+    def test_all_relations(self):
+        """Test all integrations for operator."""
+        self.harness.set_leader()
+        test_utils.set_all_pebbles_ready(self.harness)
+        # this adds all the default/common relations
+        test_utils.add_all_relations(self.harness)
+        test_utils.add_complete_ingress_relation(self.harness)
+
+        setup_cmds = [["heat-manage", "db_sync"]]
+        for cmd in setup_cmds:
+            self.assertIn(cmd, self.container_calls.execute["heat-api"])
+        config_files = ["/etc/heat/heat.conf", "/etc/heat/api-paste.ini"]
+        for f in config_files:
+            self.check_file("heat-api", f)
