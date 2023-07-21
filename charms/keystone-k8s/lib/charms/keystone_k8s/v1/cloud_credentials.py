710a84877f5502be0ed87f65a74b016031a39ba7
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2023-07-21 07:20:43 +0530

    Change cloud_credentials lib patch version to 0
    cloud_credentials v1 is not published in charmcraft
    and gives an error when published since the patch
    version is not 0.
    Changed the patch version to 0.
    
    Change-Id: I3e49df41d0f604538cf0469a4f867d55327b7df8

--- charms/keystone-k8s/lib/charms/keystone_k8s/v1/cloud_credentials.py
+++ charms/keystone-k8s/lib/charms/keystone_k8s/v1/cloud_credentials.py
@@ -97,7 +97,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 0
 
 logger = logging.getLogger(__name__)
 
