3570bc62981030ada7834b545c91c6551a8cc7bb
Guillaume Boutry <guillaume.boutry@canonical.com>
2023-09-26 17:33:45 +0200

    Migrate traefik ingress to v2
    Current ingress relation only routes unit to requirer's leader.
    Ingress V2 fixes that issue.
    
    Change-Id: I2eed63e5243afce29fa24f7a5d8976c352eedd51

--- charms/placement-k8s/fetch-libs.sh
+++ charms/placement-k8s/fetch-libs.sh
@@ -4,4 +4,4 @@ echo "INFO: Fetching libs from charmhub."
 charmcraft fetch-lib charms.data_platform_libs.v0.database_requires
 charmcraft fetch-lib charms.keystone_k8s.v1.identity_service
 charmcraft fetch-lib charms.rabbitmq_k8s.v0.rabbitmq
-charmcraft fetch-lib charms.traefik_k8s.v1.ingress
+charmcraft fetch-lib charms.traefik_k8s.v2.ingress
