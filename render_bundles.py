c15f9922fcbc3d172435050b94d9d97487034597
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2023-11-17 10:39:06 +0530

    [WIP] Add zuuljobs
    * Add sunbeam project template to run pep8 and py3 tests
    * Add zuul.d/zuul.yaml to run pep8, py3, cover tests
    * Update tox.ini for each charm to copy ops_sunbeam into lib
      folder for py3 and build.
    * Update charmcraft and requirements for each charm
    * Add global tox.ini to invoke pep8, py3 tests for modified
      charms. The global file will help in reusing existing zuul
      charm jobs without any changes.
    * Add gitreview file
    * Fix py3 test failures in ciner-ceph-k8s, glance-k8s,
      openstack-exporter
    * Add playbook to build charms. All modified charms will
      be built in parallel
    
    Change-Id: I804ca64182c109d16bd820ac00f129aa6dcf4496

---
+++ render_bundles.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#
+# Copyright 2023 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Render all smoke bundles.
+
+Renders smoke bundles with context of locally built charms.
+Prepares the context with assumption the charm is locally
+built if corresponding *.charm exists in current folder.
+"""
+
+import glob
+
+from jinja2 import (
+    Environment,
+    FileSystemLoader,
+)
+
+test_directories = ["set1"]
+built_charms = glob.glob("*.charm")
+context = {
+    charm.strip(".charm").replace("-", "_"): True for charm in built_charms
+}
+
+for test_dir in test_directories:
+    bundle_dir = f"tests/{test_dir}/bundles"
+    template_loader = Environment(loader=FileSystemLoader(bundle_dir))
+    bundle_template = template_loader.get_template("smoke.yaml.j2")
+    with open(
+        f"{bundle_dir}/smoke.yaml", mode="w", encoding="utf-8"
+    ) as content:
+        content.write(bundle_template.render(context))
