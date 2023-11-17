efbf85adbb5843103b92ed6db4f5a4408b64a4eb
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2023-11-17 10:39:06 +0530

    [WIP] Add zuuljobs
    * Add sunbeam project template to run pep8 and py3 tests
    * Add zuul.yaml to run pep8, py3, cover tests
    * Update tox.ini for each charm to copy ops_sunbeam into lib
      folder for py3 and build.
    * Update charmcraft and requirements for each charm
    * Add global tox.ini to invoke pep8, py3 tests for modified
      charms. The global file will help in reusing existing zuul
      charm jobs without any changes.
    * Add gitreview file
    
    Change-Id: I804ca64182c109d16bd820ac00f129aa6dcf4496

---
+++ run_tox.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ "$1" != "pep8" ]] && [[ "$1" != "py3" ]];
+then
+	echo "tox argument should be pep8 or py3";
+	exit 1
+fi	
+
+# Check if ops-sunbeam is changed. If so run tox on all charms.
+# Otherwise, run tox on only the charms whose code is modifed.
+ops_sunbeam_changed=$(git diff --name-only --relative=ops-sunbeam)
+if [ -z ops_sunbeam_changed ]
+then
+	modified_charms=$(git diff --name-only --relative=charms | sed -r "s/([^/]*).+/\1/" | sort -u)
+else
+	modified_charms=$(ls charms)
+fi
+
+for charm in $modified_charms; do pushd charms/$charm; tox -e $1; popd; done
