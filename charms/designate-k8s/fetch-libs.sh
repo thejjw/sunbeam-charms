562385b9ad23ac52011cb5a30785043c492929d1
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2023-11-17 10:39:06 +0530

    Add zuuljobs
    * Add sunbeam project template to run pep8, py3 tests
    * Add zuul.d/zuul.yaml to run pep8, py3, cover tests
    * Update charmcraft and requirements for each charm
    * Add global tox.ini to invoke fmt, pep8, py3, cover,
      build
    * Add gitreview file
    * Fix py3 test failures in ciner-ceph-k8s, glance-k8s,
      openstack-exporter
    * Add jobs for charm builds using files option so that
      job is invoked if files within the component are
      modified. Add charm builds to both check and gate
      pipeline.
    * Make function tests as part of global. Split the function
      tests into core, ceph, caas, misc mainly to accomodate
      function tests to run on 8GB. Add function tests as
      part of check pipeline.
    * Add zuul job to publish charms in promote pipeline
      Add charmhub token as secret that can be used to
      publish charms.
      Note: Charmhub token is generated with ttl of 90 days.
    * Run tox formatting
    * Make .gitignore, .jujuignore, .stestr.conf global and
      remove the files from all charms.
    * Make libs and templates global. Split libs to internal
      and external so that internal libs can adhere to
      sunbeam formatting styles.
    * Add script to copy common files necessary libs, config
      templates, stestr conf, jujuignore during py3 tests
      and charm builds.
    * Tests for keystone-ldap-k8s are commented due to
      intermittent bug LP#2045206
    
    Change-Id: I804ca64182c109d16bd820ac00f129aa6dcf4496

--- charms/designate-k8s/fetch-libs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "INFO: Fetching libs from charmhub."
-charmcraft fetch-lib charms.data_platform_libs.v0.database_requires
-charmcraft fetch-lib charms.keystone_k8s.v1.identity_service
-charmcraft fetch-lib charms.rabbitmq_k8s.v0.rabbitmq
-charmcraft fetch-lib charms.traefik_k8s.v2.ingress
-charmcraft fetch-lib charms.designate_bind_k8s.v0.bind_rndc
-
