a9e764060b05bfc8a655ae0f02c15b53f60f2de8
Chi Wai Chan <chiwai.chan@canonical.com>
2023-12-12 18:04:32 +0800

    Add tempest-k8s charm
    Add a charm for running tempest against a sunbeam openstack deployment.
    It has support for running a "validate" action with parameters
    passed to tempest.
    Further development is planned, including periodic (cron based) tempest
    checks and integration with COS.
    Initial stubs are included to support this development.
    
    Co-authored-by: Samuel Allan <samuel.allan@canonical.com>
    
    Change-Id: Ie66177489ae38355a8f0025006192cc409e0e761

---
+++ charms/tempest-k8s/src/utils/constants.py
@@ -0,0 +1,32 @@
+# Copyright 2024 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Constants for the tempest charm."""
+CONTAINER = "tempest"
+
+TEMPEST_CONCURRENCY = "4"
+TEMPEST_HOME = "/var/lib/tempest"
+TEMPEST_WORKSPACE_PATH = f"{TEMPEST_HOME}/workspace"
+TEMPEST_CONF = f"{TEMPEST_WORKSPACE_PATH}/etc/tempest.conf"
+TEMPEST_TEST_ACCOUNTS = f"{TEMPEST_WORKSPACE_PATH}/test_accounts.yaml"
+TEMPEST_LIST_DIR = "/tempest_test_lists"
+# this file will contain the output from tempest's latest test run
+TEMPEST_OUTPUT = f"{TEMPEST_WORKSPACE_PATH}/tempest-output.log"
+# This is the workspace name registered with tempest.
+# It will be saved in a file in $HOME/.tempest/
+TEMPEST_WORKSPACE = "tempest"
+
+OPENSTACK_USER = "tempest"
+OPENSTACK_DOMAIN = "tempest"
+OPENSTACK_PROJECT = "tempest-CloudValidation"
+OPENSTACK_ROLE = "admin"
