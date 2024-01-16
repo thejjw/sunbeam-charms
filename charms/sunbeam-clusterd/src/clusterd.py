d3ac4f55143b60d127b568bc960ba8fcb191214b
Guillaume Boutry <guillaume.boutry@canonical.com>
2024-01-16 17:20:14 +0100

    Implement sunbeam-clusterd
    Sunbeam-clusterd can scale up and down.
    Currently, the external address in bound to the peers relationship.
    Exposes the action `get-credentials` which returns the URL. In the long
    term, it will return the credentials to access securely clusterd.
    
    Change-Id: I8b91efe6d96198f5ad3634b9747161225381ded6

---
+++ charms/sunbeam-clusterd/src/clusterd.py
@@ -0,0 +1,138 @@
+# Copyright 2024 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Clusterd client talking over unix socket."""
+
+import json
+import logging
+from pathlib import (
+    Path,
+)
+from urllib.parse import (
+    quote,
+)
+
+import requests
+import requests_unixsocket
+from requests_unixsocket import (
+    DEFAULT_SCHEME,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class ClusterdUnavailableError(Exception):
+    """Raised when the cluster is unavailable."""
+
+
+class ClusterdClient:
+    """A client for interacting with the remote client API."""
+
+    def __init__(self, socket_path: Path):
+        self._socket_path = socket_path
+        self._session = requests.sessions.Session()
+        self._session.mount(
+            requests_unixsocket.DEFAULT_SCHEME,
+            requests_unixsocket.UnixAdapter(),
+        )
+
+    def _request(self, method: str, path: str, **kwargs) -> dict:
+        if path.startswith("/"):
+            path = path[1:]
+        netloc = quote(str(self._socket_path), safe="")
+        url = f"{DEFAULT_SCHEME}{netloc}/{path}"
+        try:
+            logging.debug("[%s] %s, args=%s", method, url, kwargs)
+            response = self._session.request(method=method, url=url, **kwargs)
+            logging.debug("Response(%s) = %s", response, response.text)
+        except requests.exceptions.ConnectionError as e:
+            msg = str(e)
+            if "FileNotFoundError" in msg:
+                raise ClusterdUnavailableError(
+                    "Sunbeam Cluster socket not found, is clusterd running ?"
+                    " Check with 'snap services openstack.clusterd'",
+                ) from e
+            raise ClusterdUnavailableError(msg)
+        except requests.exceptions.HTTPError as e:
+            if e.response is not None:
+                logger.debug(
+                    f"HTTPError: {e.response.status_code}, {e.response.text}"
+                )
+                if e.response.status_code == 503:
+                    raise ClusterdUnavailableError(str(e)) from e
+            raise e
+        response.raise_for_status()
+        return response.json()
+
+    def _get(self, path, **kwargs):
+        kwargs.setdefault("allow_redirects", True)
+        return self._request("get", path, **kwargs)
+
+    def _post(self, path, data=None, json=None, **kwargs):
+        return self._request("post", path, data=data, json=json, **kwargs)
+
+    def _delete(self, path, **kwargs):
+        return self._request("delete", path, **kwargs)
+
+    def ready(self) -> bool:
+        """Is the cluster ready."""
+        try:
+            self._get("cluster/1.0/ready")
+        except ClusterdUnavailableError:
+            return False
+        return True
+
+    def shutdown(self):
+        """Shutdown local clusterd."""
+        self._post("cluster/control/shutdown")
+
+    def bootstrap(self, name: str, address: str):
+        """Bootstrap clusterd."""
+        data = {"bootstrap": True, "address": address, "name": name}
+        self._post("/cluster/control", data=json.dumps(data))
+
+    def join(self, name: str, address: str, token: str) -> None:
+        """Join node to the micro cluster.
+
+        Verified the token with the list of saved tokens and
+        joins the node with the given name and address.
+        """
+        data = {"join_token": token, "address": address, "name": name}
+        self._post("cluster/control", data=json.dumps(data))
+
+    def get_members(self) -> list[dict]:
+        """Get cluster members."""
+        cluster = self._get("/cluster/1.0/cluster")["metadata"]
+        return cluster
+
+    def remove_node(self, name: str, allow_not_found: bool = True):
+        """Delete node."""
+        try:
+            self._delete(f"/cluster/1.0/cluster/{name}")
+        except requests.exceptions.HTTPError as e:
+            if e.response is None:
+                raise e
+            if e.response.status_code == 404 and allow_not_found:
+                logger.debug(f"Node {name} not found")
+                return
+            raise e
+
+    def generate_token(self, name: str) -> str:
+        """Generate token for the node.
+
+        Generate a new token for the node with name.
+        """
+        data = {"name": name}
+        result = self._post("/cluster/1.0/tokens", data=json.dumps(data))
+        return str(result["metadata"])
