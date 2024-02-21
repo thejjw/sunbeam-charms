033e1af7f5f91b01346c840bc3fe6535f93fe5fb
Samuel Allan <samuel.allan@canonical.com>
2024-02-21 12:35:59 +1030

    Add tempest-k8s charm to the core func test bundle
    This is to verify that the tempest-k8s charm deploys successfully
    (reaches active/idle, the tempest environment was set up successfully),
    and doesn't adversely impact the rest of the sunbeam deployment.
    
    Also add some initial functional tests run with zaza.
    
    Change-Id: I548eb07ed1eb9e67c46f62a16d36bfaec83d7040

---
+++ tests/local/zaza/sunbeam/charm_tests/tempest_k8s/tests.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2024 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import subprocess
+import unittest
+from random import shuffle
+from typing import Tuple
+
+import requests
+import tenacity
+import zaza.model as model
+import zaza.openstack.charm_tests.test_utils as test_utils
+
+
+class TempestK8sTest(test_utils.BaseCharmTest):
+    """Charm tests for tempest-k8s."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Run class setup for running tests."""
+        super(TempestK8sTest, cls).setUpClass(
+            application_name="tempest"
+        )
+
+    def test_get_lists(self):
+        """Verify that the get-lists action returns list names as expected."""
+        action = model.run_action_on_leader(
+            self.application_name, "get-lists"
+        )
+        lists = action.data["results"]["stdout"].splitlines()
+        self.assertIn("readonly-quick", lists)
+        self.assertIn("refstack-2022.11", lists)
+
+    def test_validate_with_readonly_quick_tests(self):
+        """Verify that the validate action runs tests as expected."""
+        action = model.run_action_on_leader(
+            self.application_name, "validate",
+            action_params={
+                "test-list": "readonly-quick",
+            }
+        )
+        summary = action.data["results"]["summary"]
+
+        # These are the expected results with the test bundle;
+        self.assertIn("Ran: 23 tests", summary)
+        self.assertIn("Passed: 19", summary)
+        self.assertIn("Skipped: 4", summary)
