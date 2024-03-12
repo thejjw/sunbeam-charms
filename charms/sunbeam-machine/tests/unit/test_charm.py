9378a06a0a9461b3c5cfc34a1314343b7d6946c0
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2024-03-12 14:46:54 +0530

    [sunbeam-machine] Add proxy configs
    Add http_proxy, https_proxy, no_proxy as
    configuration to charm. Update the content
    of /etc/environment file on setting the
    proxy configs.
    If the proxy configuration is not set on
    sunbeam-machine, clear the corresponding
    configs from /etc/environment.
    
    Change-Id: I5ba444d87e199a4314439eddf513729138c40c3f

--- charms/sunbeam-machine/tests/unit/test_charm.py
+++ charms/sunbeam-machine/tests/unit/test_charm.py
@@ -14,6 +14,11 @@
 
 """Tests for Sunbeam Machine charm."""
 
+from unittest.mock import (
+    mock_open,
+    patch,
+)
+
 import charm
 import ops_sunbeam.test_utils as test_utils
 
@@ -30,7 +35,7 @@ class _SunbeamMachineCharm(charm.SunbeamMachineCharm):
 class TestCharm(test_utils.CharmTestCase):
     """Classes for testing Sunbeam Machine charm."""
 
-    PATCHES = []
+    PATCHES = ["sysctl"]
 
     def setUp(self):
         """Setup Sunbeam machine tests."""
@@ -46,5 +51,108 @@ class TestCharm(test_utils.CharmTestCase):
 
     def test_initial(self):
         """Bootstrap test initial."""
-        self.harness.begin_with_initial_hooks()
+        file_content_dict = {"PATH": "FAKEPATH"}
+        env_file_content = "\n".join(
+            f"{k}={v}" for k, v in file_content_dict.items()
+        )
+
+        with patch(
+            "builtins.open", new_callable=mock_open, read_data=env_file_content
+        ) as mock_file:
+            self.harness.begin_with_initial_hooks()
+            mock_file().write.assert_not_called()
+
         self.assertTrue(self.harness.charm.bootstrapped())
+
+    def test_proxy_settings(self):
+        """Test setting proxies."""
+        # test_data is a tuple of /etc/environment file content as dict, proxy config as dict,
+        # expected content as dict
+        # As the below tests are run in loop as subtests, they act as juju config commands.
+        # Means the configs set in the previous test data remains until it is reset by
+        # specifying config as empty string.
+        test_data = [
+            # Case 1: No proxy in environment file, set http_proxy, https_proxy
+            (
+                {"PATH": "FAKEPATH"},
+                {
+                    "http_proxy": "http://proxyserver:3128",
+                    "https_proxy": "http://proxyserver:3128",
+                },
+                {
+                    "PATH": "FAKEPATH",
+                    "HTTP_PROXY": "http://proxyserver:3128",
+                    "HTTPS_PROXY": "http://proxyserver:3128",
+                },
+            ),
+            # Case 2: Add no_proxy to above configuration
+            (
+                {
+                    "PATH": "FAKEPATH",
+                    "HTTP_PROXY": "http://proxyserver:3128",
+                    "HTTPS_PROXY": "http://proxyserver:3128",
+                },
+                {"no_proxy": "localhost,127.0.0.1"},
+                {
+                    "PATH": "FAKEPATH",
+                    "HTTP_PROXY": "http://proxyserver:3128",
+                    "HTTPS_PROXY": "http://proxyserver:3128",
+                    "NO_PROXY": "localhost,127.0.0.1",
+                },
+            ),
+            # Case 3: Update http proxy to different value
+            (
+                {
+                    "PATH": "FAKEPATH",
+                    "HTTP_PROXY": "http://proxyserver:3128",
+                    "HTTPS_PROXY": "http://proxyserver:3128",
+                    "NO_PROXY": "localhost,127.0.0.1",
+                },
+                {
+                    "http_proxy": "http://proxyserver:3120",
+                    "https_proxy": "http://proxyserver:3128",
+                },
+                {
+                    "PATH": "FAKEPATH",
+                    "HTTP_PROXY": "http://proxyserver:3120",
+                    "HTTPS_PROXY": "http://proxyserver:3128",
+                    "NO_PROXY": "localhost,127.0.0.1",
+                },
+            ),
+            # Case 4: Reset the no_proxy config
+            (
+                {
+                    "PATH": "FAKEPATH",
+                    "HTTP_PROXY": "http://proxyserver:3120",
+                    "HTTPS_PROXY": "http://proxyserver:3128",
+                    "NO_PROXY": "localhost,127.0.0.1",
+                },
+                {"no_proxy": ""},
+                {
+                    "PATH": "FAKEPATH",
+                    "HTTP_PROXY": "http://proxyserver:3120",
+                    "HTTPS_PROXY": "http://proxyserver:3128",
+                },
+            ),
+        ]
+
+        with patch(
+            "builtins.open", new_callable=mock_open, read_data=""
+        ) as mock_file:
+            self.harness.begin_with_initial_hooks()
+
+        for index, d in enumerate(test_data):
+            with self.subTest(msg=f"test_proxy_settings-{index}", data=d):
+                env_file_content = "\n".join(
+                    f"{k}={v}" for k, v in d[0].items()
+                )
+                expected_content = "\n".join(
+                    f"{k}={v}" for k, v in d[2].items()
+                )
+                with patch(
+                    "builtins.open",
+                    new_callable=mock_open,
+                    read_data=env_file_content,
+                ) as mock_file:
+                    self.harness.update_config(d[1])
+                    mock_file().write.assert_called_with(expected_content)
