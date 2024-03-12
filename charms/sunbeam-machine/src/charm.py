ada047a90f424985ecf6b049a4b0b5d82b3340be
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2024-03-12 14:46:54 +0530

    [sunbeam-machine] Add proxy configs
    Add http_proxy, https_proxy, no_proxy as
    configuration to charm. Update the content
    of /etc/environment file on setting the
    proxy configs.
    If the proxy configuration is not set on
    sunbeam-machine, clear the corresponding
    configs from /etc/environment.
    
    Change-Id: I5ba444d87e199a4314439eddf513729138c40c3f

--- charms/sunbeam-machine/src/charm.py
+++ charms/sunbeam-machine/src/charm.py
@@ -33,6 +33,7 @@ from ops.main import (
     main,
 )
 
+ETC_ENVIRONMENT = "/etc/environment"
 logger = logging.getLogger(__name__)
 
 
@@ -41,6 +42,7 @@ class SunbeamMachineCharm(sunbeam_charm.OSBaseOperatorCharm):
 
     _state = ops.framework.StoredState()
     service_name = "sunbeam-machine"
+    proxy_configs = ["http_proxy", "https_proxy", "no_proxy"]
 
     def __init__(self, framework: ops.Framework) -> None:
         super().__init__(framework)
@@ -63,6 +65,34 @@ class SunbeamMachineCharm(sunbeam_charm.OSBaseOperatorCharm):
         except sysctl.CommandError:
             logger.error("Error executing sysctl", exc_info=True)
             raise sunbeam_guard.BlockedExceptionError("Sysctl command failed")
+
+    def _on_config_changed(self, event: ops.ConfigChangedEvent):
+        self.configure_charm(event)
+        with open(ETC_ENVIRONMENT, mode="r", encoding="utf-8") as file:
+            current_env = dict(
+                line.strip().split("=", 1) for line in file if "=" in line
+            )
+            logger.info(f"Existing content of /etc/environment: {current_env}")
+
+        proxy = {p: v for p in self.proxy_configs if (v := self.config.get(p))}
+        if all(
+            proxy.get(p) == current_env.get(p.upper())
+            for p in self.proxy_configs
+        ):
+            return
+
+        # Remove proxies not set
+        not_set_proxies = self.proxy_configs - proxy.keys()
+        for p in not_set_proxies:
+            if (p_upper := p.upper()) in current_env:
+                del current_env[p_upper]
+
+        # Capitalise proxy keys and update env
+        proxy_in_caps = {k.upper(): v for k, v in proxy.items()}
+        current_env.update(proxy_in_caps)
+
+        with open(ETC_ENVIRONMENT, mode="w", encoding="utf-8") as file:
+            file.write("\n".join([f"{k}={v}" for k, v in current_env.items()]))
 
     def _on_remove(self, event: ops.RemoveEvent):
         self.sysctl.remove()
