1fb7fb8496f33ae6d057b583dc231be09fd6929f
Samuel Allan <samuel.allan@canonical.com>
2024-06-11 17:14:33 +0930

    WIP Improve tempest alert rules
    - alert if results not seen for some time
    - be more accurate about the last test results
    
    Change-Id: I4280cd181e709cef3bba6a114273cb32c12a9f7d

--- charms/tempest-k8s/src/charm.py
+++ charms/tempest-k8s/src/charm.py
@@ -21,6 +21,9 @@ This charm provide Tempest as part of an OpenStack deployment
 
 import logging
 import os
+from functools import (
+    cache,
+)
 from typing import (
     Dict,
     List,
@@ -51,6 +54,10 @@ from ops.model import (
 from ops_sunbeam.config_contexts import (
     ConfigContext,
 )
+from utils.alert_rules import (
+    ensure_alert_rules_disabled,
+    update_alert_rules_files,
+)
 from utils.cleanup import (
     CleanUpError,
     run_extensive_cleanup,
@@ -73,6 +80,7 @@ from utils.types import (
     TempestEnvVariant,
 )
 from utils.validators import (
+    Schedule,
     validated_schedule,
 )
 
@@ -85,7 +93,11 @@ class TempestConfigurationContext(ConfigContext):
     def context(self) -> dict:
         """Tempest context."""
         return {
-            "schedule": self.charm.get_schedule(),
+            "schedule": (
+                self.charm.get_schedule().value
+                if self.charm.is_schedule_ready()
+                else ""
+            ),
         }
 
 
@@ -131,31 +143,25 @@ class TempestOperatorCharm(sunbeam_charm.OSBaseOperatorCharmK8S):
             ),
         ]
 
-    def get_schedule(self) -> str:
-        """Return the schedule option if valid and should be enabled.
+    @cache
+    def get_schedule(self) -> Schedule:
+        """Validate and return the schedule from config."""
+        return validated_schedule(self.config["schedule"])
 
-        If the schedule option is invalid,
-        or periodic checks shouldn't currently be enabled
-        (eg. observability relations not ready),
-        then return an empty schedule string.
-        An empty string disables the schedule.
+    def is_schedule_ready(self) -> bool:
+        """Check if the schedule is valid and periodic tests should be enabled.
+
+        Return True if the schedule config option is valid,
+        and pre-requisites for periodic checks are ready.
         """
-        schedule = validated_schedule(self.config["schedule"])
-        if not schedule.valid:
-            return ""
-
-        # if tempest env isn't ready,
-        # or if the logging relation isn't joined,
-        # or if keystone isn't ready,
-        # then we can't start scheduling periodic tests
-        if not (
-            self.is_tempest_ready()
+        schedule = self.get_schedule()
+        return (
+            schedule.valid
+            and schedule.value
+            and self.is_tempest_ready()
             and self.loki.ready
             and self.user_id_ops.ready
-        ):
-            return ""
-
-        return schedule.value
+        )
 
     @property
     def config_contexts(self) -> List[ConfigContext]:
@@ -322,7 +328,7 @@ class TempestOperatorCharm(sunbeam_charm.OSBaseOperatorCharmK8S):
 
         logger.info("Configuring the tempest environment")
 
-        schedule = validated_schedule(self.config["schedule"])
+        schedule = self.get_schedule()
         if not schedule.valid:
             raise sunbeam_guard.BlockedExceptionError(
                 f"invalid schedule config: {schedule.err}"
@@ -339,6 +345,14 @@ class TempestOperatorCharm(sunbeam_charm.OSBaseOperatorCharmK8S):
             raise sunbeam_guard.BlockedExceptionError(
                 "tempest init failed, see logs for more info"
             )
+
+        if self.is_schedule_ready():
+            update_alert_rules_files(schedule)
+        else:
+            ensure_alert_rules_disabled()
+
+        if self.loki.ready:
+            self.loki.interface._reinitialize_alert_rules()
 
         self.status.set(ActiveStatus(""))
         logger.info("Finished configuring the tempest environment")
---
+++ charms/tempest-k8s/src/utils/alert_rules.py
@@ -0,0 +1,82 @@
+# Copyright 2024 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Working with the loki logging alert rules."""
+import os
+
+import yaml
+from utils.validators import (
+    Schedule,
+)
+
+ALERT_RULES_PATH = "src/loki_alert_rules"
+ALERT_RULES_FILE = ALERT_RULES_PATH + "/tests.rules"
+
+# The default for max_query_length in Loki is now 721h,
+# and thus the value in Loki deployed by COS.
+# ref. https://github.com/grafana/loki/issues/4509
+# We need a small buffer to make it work in these queries.
+MAX_RANGE_HOURS = 719
+
+
+def ensure_alert_rules_disabled():
+    """Ensure the alert rules files don't exist."""
+    try:
+        os.remove(ALERT_RULES_FILE)
+    except FileNotFoundError:
+        pass
+    return
+
+
+def update_alert_rules_files(schedule: Schedule) -> None:
+    """Update files for alert rules based on the schedule.
+
+    `schedule` is expected to be a valid and ready Schedule.
+    """
+    absent_range_hours = min(
+        # Convert seconds to hours,
+        # and double the interval to ensure it only alerts when one was definitely missed.
+        int(schedule.max_interval / 60 / 60) * 2,
+        # Ensure that the log query limit isn't exceeded
+        MAX_RANGE_HOURS,
+    )
+
+    rules = {
+        "groups": [
+            {
+                "name": "tempest-failed-tests",
+                "rules": [
+                    {
+                        "alert": "FailedTests",
+                        "expr": f'last_over_time({{filename="/var/lib/tempest/workspace/tempest-periodic.log", %%juju_topology%%}} |~ "- Failed:" | pattern " - <_>: <number_of_tests>" | unwrap number_of_tests [{MAX_RANGE_HOURS}h]) > 0',
+                        "labels": {
+                            "severity": "high",
+                        },
+                    },
+                    {
+                        "alert": "AbsentTests",
+                        "expr": f'absent_over_time({{filename="/var/lib/tempest/workspace/tempest-periodic.log", %%juju_topology%%}} |~ "- Failed:" [{absent_range_hours}h]) == 1',
+                        "labels": {
+                            "severity": "high",
+                        },
+                        "annotations": {
+                            "summary": "Tempest periodic tests not seen recently",
+                        },
+                    },
+                ],
+            }
+        ]
+    }
+
+    with open(ALERT_RULES_FILE, "w") as f:
+        yaml.safe_dump(rules, f)
