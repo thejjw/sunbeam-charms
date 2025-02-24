99e69fdc9d350c1247142f6474c6985620334d4b
Guillaume Boutry <guillaume.boutry@canonical.com>
2025-02-24 11:06:54 +0100

    [ops-sunbeam] Allow post-init to throw status exceptions
    When the setup of relation handlers throws an ops sunbeam status
    exception, the charm is put to error while this is a supported patterns
    for developping charms. The reason is that the exception is not thrown
    from within a guard. But it is reasonable, for example, for
    `OSBaseOperatorAPICharm.internal_url` to raise a WaitingExceptionError
    instead of returning None.
    
    Change-Id: Ide137421308733784b6aca7e247eb3e13485d2ff
    Signed-off-by: Guillaume Boutry <guillaume.boutry@canonical.com>

--- ops-sunbeam/ops_sunbeam/core.py
+++ ops-sunbeam/ops_sunbeam/core.py
@@ -25,6 +25,9 @@ from typing import (
 )
 
 import ops_sunbeam.tracing as sunbeam_tracing
+from ops_sunbeam.guard import (
+    BaseStatusExceptionError,
+)
 
 if TYPE_CHECKING:
     from ops_sunbeam.charm import (
@@ -101,5 +104,20 @@ class PostInitMeta(type):
     def __call__(cls, *args, **kw):
         """Call __post_init__ after __init__."""
         instance = super().__call__(*args, **kw)
-        instance.__post_init__()
+        try:
+            instance.__post_init__()
+        except BaseStatusExceptionError as e:
+            # Allow post init to raise an ops_sunbeam status
+            # exception without causing the charm to error.
+            # This status will be collected and set on the
+            # unit.
+            # import here to avoid circular import
+            from ops_sunbeam.charm import (
+                OSBaseOperatorCharm,
+            )
+
+            if isinstance(instance, OSBaseOperatorCharm):
+                instance.status.set(e.to_status())
+            else:
+                raise e
         return instance
