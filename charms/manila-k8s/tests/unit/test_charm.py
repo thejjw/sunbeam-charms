be593377f7199721f555215666a72b0948a77174
Claudiu Belu <cbelu@cloudbasesolutions.com>
2025-05-29 13:09:11 +0000

    WIP: adds manila controller services
    Change-Id: I7e9c075e266326060eebbd3db08123d178a7fa36

---
+++ charms/manila-k8s/tests/unit/test_charm.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for the Manila K8s charm."""
+
+import charm
+import ops_sunbeam.test_utils as test_utils
+
+
+class _ManilaOperatorCharm(charm.ManilaOperatorCharm):
+    """Test implementation of Manila operator."""
+
+    def __init__(self, framework):
+        self.seen_events = []
+        self.render_calls = []
+        super().__init__(framework)
+
+    def _log_event(self, event):
+        self.seen_events.append(type(event).__name__)
+
+    def renderer(self, containers, container_configs, template_dir, adapters):
+        """Intercept and record all calls to render config files."""
+        self.render_calls.append(
+            (containers, container_configs, template_dir, adapters)
+        )
+
+    def configure_charm(self, event):
+        """Intercept and record full charm configuration events."""
+        super().configure_charm(event)
+        self._log_event(event)
+
+    @property
+    def public_ingress_address(self):
+        return "manila.juju"
+
+
+class TestManilaOperatorCharm(test_utils.CharmTestCase):
+    """Unit tests for Manila Operator."""
+
+    PATCHES = []
+
+    def setUp(self):
+        """Setup test fixtures for test."""
+        self.container_calls = {"push": {}, "pull": [], "remove_path": []}
+        super().setUp(charm, self.PATCHES)
+        self.harness = test_utils.get_harness(
+            _ManilaOperatorCharm, container_calls=self.container_calls
+        )
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
+    def test_pebble_ready_handler(self):
+        """Test pebble ready event handling."""
+        self.assertEqual(self.harness.charm.seen_events, [])
+        self.harness.container_pebble_ready("manila-api")
+        self.assertEqual(self.harness.charm.seen_events, ["PebbleReadyEvent"])
