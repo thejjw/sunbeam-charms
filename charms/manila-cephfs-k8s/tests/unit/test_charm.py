9859b273cb470169395daa5cac67cc1a1cfc84f3
Claudiu Belu <cbelu@cloudbasesolutions.com>
2025-06-03 15:01:57 +0000

    Adds manila-cephfs-k8s ceph-nfs relation
    The relation can used to relate to a ceph-nfs-client provider
    (e.g. microceph), and can be used to setup the CephFSDriver.
    
    Change-Id: I94bffc3a7ab8bf36ffe7a8507b8e78c4adbcaa31
    Signed-off-by: Claudiu Belu <cbelu@cloudbasesolutions.com>
    (cherry picked from commit 508db925f97636a7fa958e71a3752528211511fa)

--- charms/manila-cephfs-k8s/tests/unit/test_charm.py
+++ charms/manila-cephfs-k8s/tests/unit/test_charm.py
@@ -18,6 +18,10 @@
 
 import charm
 import ops_sunbeam.test_utils as test_utils
+from ops import (
+    model,
+    pebble,
+)
 
 
 class _ManilaCephfsCharm(charm.ManilaShareCephfsCharm):
@@ -57,6 +61,45 @@ class TestManilaCephfsCharm(test_utils.CharmTestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
+    def add_ceph_nfs_client_relation(self) -> None:
+        """Add the ceph-nfs relation and unit data."""
+        app_data = {
+            "client": "client.foo",
+            "cluster-id": "lish",
+            "fsid": "fake-fsid",
+            "keyring": "keys-do-not-ring",
+            "mon-hosts": '["mony"]',
+            "volume": "voly",
+        }
+        return self.harness.add_relation(
+            charm.CEPH_NFS_RELATION_NAME,
+            "microceph",
+            app_data=app_data,
+        )
+
+    def _file_exists(self, container, path) -> bool:
+        client = self.harness.charm.unit.get_container(container)._pebble  # type: ignore
+
+        try:
+            files = client.list_files(path, itself=True)
+        except pebble.APIError as ex:
+            if ex.code == 404:
+                return False
+
+            # Reraise if there's any other error than Not Found.
+            raise
+
+        return len(files) == 1 and files[0].path == path
+
+    def _check_file_contents(self, container, path, strings):
+        client = self.harness.charm.unit.get_container(container)._pebble  # type: ignore
+
+        with client.pull(path) as infile:
+            received_data = infile.read()
+
+        for string in strings:
+            self.assertIn(string, received_data)
+
     def test_pebble_ready_handler(self):
         """Test pebble ready event handling."""
         self.assertEqual(self.harness.charm.seen_events, [])
@@ -70,3 +113,60 @@ class TestManilaCephfsCharm(test_utils.CharmTestCase):
 
         # this adds all the default/common relations
         test_utils.add_all_relations(self.harness)
+
+        # ceph-nfs is a required relation.
+        ceph_nfs_status = self.harness.charm.ceph_nfs.status
+        self.assertIsInstance(ceph_nfs_status.status, model.BlockedStatus)
+
+        # The files should not exist if the relation is not present.
+        self.assertFalse(
+            self._file_exists("manila-share", "/etc/ceph/ceph.conf")
+        )
+        self.assertFalse(
+            self._file_exists("manila-share", "/etc/ceph/manila.keyring")
+        )
+        self.assertFalse(
+            self._file_exists("manila-share", "/etc/manila/manila.conf")
+        )
+
+        ceph_rel_id = self.add_ceph_nfs_client_relation()
+
+        # Now that the relation is added, we should have the ceph-related
+        # options set.
+        ceph_conf_strings = [
+            "[global]",
+            "mon host = mony",
+            "[client]",
+            "keyring = /etc/ceph/manila.keyring",
+        ]
+        self._check_file_contents(
+            "manila-share",
+            "/etc/ceph/ceph.conf",
+            ceph_conf_strings,
+        )
+        keyring_strings = ["[client.foo]", "key = keys-do-not-ring"]
+        self._check_file_contents(
+            "manila-share",
+            "/etc/ceph/manila.keyring",
+            keyring_strings,
+        )
+        manila_strings = [
+            "enabled_share_backends = cephnfs",
+            "[cephnfs]",
+            "share_backend_name = CEPHNFS",
+            "cephfs_auth_id = foo",
+            "cephfs_cluster_name = lish",
+            "cephfs_nfs_cluster_id = lish",
+            "cephfs_filesystem_name = voly",
+        ]
+        self._check_file_contents(
+            "manila-share",
+            "/etc/manila/manila.conf",
+            manila_strings,
+        )
+
+        # Remove the ceph-nfs relation. The relation handler should be in a
+        # BlockedStatus.
+        self.harness.remove_relation(ceph_rel_id)
+
+        self.assertIsInstance(ceph_nfs_status.status, model.BlockedStatus)
