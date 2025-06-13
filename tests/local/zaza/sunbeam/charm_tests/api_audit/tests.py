fd5307e620e5782bb8be0b38613e14d8d4c51aa5
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2025-06-13 11:17:45 +0300

    keystone: always log CADF audit messages
    Right now, the CADF audit messages will not be logged unless
    telemetry is enabled.
    
    We'll fix this by using the "log" oslo_messaging_notifications
    driver if telemetry is disabled, ensuring that the CADF events
    will always be logged.
    
    The tests are updated accordingly, testing both situations.
    
    Change-Id: I0bcdcd8b5477d04c6bd0d8d5cc691e4b5ca7edcf

--- tests/local/zaza/sunbeam/charm_tests/api_audit/tests.py
+++ tests/local/zaza/sunbeam/charm_tests/api_audit/tests.py
@@ -70,26 +70,38 @@ class OpenStackAPIAuditTest(test_utils.BaseCharmTest):
 class KeystoneAPIAuditTest(OpenStackAPIAuditTest):
     application_name = "keystone"
 
-    def _trigger_audit_event(self):
-        # We'll update the user email to trigger an audit event.
-        auth = openstack_utils.get_overcloud_auth()
-        username = auth["OS_USERNAME"]
-        user = self.keystone_client.users.find(name=username)
-
-        rand_num = random.randint(0, 32768)
-        email = f"test{rand_num}.example.com"
-        self.keystone_client.users.update(user.id, email=email)
-
     def test_audit(self):
         # Expects the following relation: rabbitmq:amqp keystone:amqp
         default_config = {'enable-telemetry-notifications': False}
         alternate_config = {'enable-telemetry-notifications': True}
+
+        rand_num = random.randint(0, 32768)
+        name = f"test-{rand_num}"
+
+        # Check that audit messages are logged with telemetry enabled.
+        # We expect the "keystone-audit-monitor" service to receive
+        # and log the CADF events.
         with self.config_change(
                 default_config=default_config,
                 alternate_config=alternate_config,
                 application_name="keystone"):
+            # Create a domain to trigger an event.
+            self.keystone_client.domains.create(name)
             exp_msg = (
-                "oslo.messaging.notification.identity.user.updated")
+                "oslo.messaging.notification.identity.domain.created")
+            self.check_audit_logs(exp_msg)
+
+        # Check that audit messages are logged with telemetry disabled.
+        # We expect the "wsgi-keystone" service to receive and log the
+        # CADF events.
+        with self.config_change(
+                default_config=alternate_config,
+                alternate_config=default_config,
+                application_name="keystone"):
+            # Delete the newly created domain and check the event.
+            self.keystone_client.domains.delete(name)
+            exp_msg = (
+                "oslo.messaging.notification.identity.domain.created")
             self.check_audit_logs(exp_msg)
 
 
