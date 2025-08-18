aaae8a933ad9f0fa993fb916c3807e66fdaf0e46
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2025-08-18 11:51:36 +0000

    Add DPDK configuration
    This change adds the following DPDK related settings:
    
    * dpdk-enabed - enable OVS DPDK data path
    * dpdk-datapath-cores - the number of cores to use for the DPDK
      data path
    * dpdk-control-plane-cores - the number of cores to use for the
      DPDK data path
    * dpdk-memory - the amount of memory in MB to allocate from
      huge pages to OVS DPDK
    
    The DPDK resources are reserved using the EPA service, taking
    into account the NUMA nodes of the network interfaces used with DPDK.
    
    The user may also override the DPDK configuration through
    `/etc/sunbeam/dpdk.yaml`.
    
    Physical ports will be handled in a subsequent PR.
    
    Change-Id: I956eb54119efa30b7cf8eb8b225a0e6a9d0cb84b
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

---
+++ charms/openstack-hypervisor/src/schemas.py
@@ -0,0 +1,35 @@
+# Copyright 2025 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Schemas used by the openstack-hypervisor charm."""
+
+DPDK_CONFIG_OVERRIDE_SCHEMA = """
+type: object
+required: [dpdk]
+additionalProperties: false
+properties:
+    dpdk:
+        type: object
+        required: [dpdk-enabled]
+        additionalProperties: false
+        properties:
+            dpdk-enabled:
+                type: boolean
+            dpdk-memory:
+                type: integer
+            dpdk-datapath-cores:
+                type: integer
+            dpdk-controlplane-cores:
+                type: integer
+"""
