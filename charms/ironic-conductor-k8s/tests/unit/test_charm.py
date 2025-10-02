c3326f4bf4d3cfbce4e80fb8d87a8e1fa5353317
Claudiu Belu <cbelu@cloudbasesolutions.com>
2025-10-02 13:48:33 +0000

    ironic-conductor-k8s: Adds set-temp-url-secret action
    The action is meant to be run on the leader after the mandatory
    relations are ready. The action will check that the Glance and
    Swift endpoints exist, and that Glance has the Swift backend
    configured. The action will store the "x-account-meta-temp-url-key"
    in Swift, if needed.
    
    Changes from the original commit:
    - Updated pyproject.toml constraint-dependencies to the 2024.1
      upper-constraints.
    - Updated charms/ironic-conductor-k8s/uv.lock (ran `uv lock`).
    
    Change-Id: I4021ba1be507251219ae821eb8f840ca5ed97f32
    Signed-off-by: Claudiu Belu <cbelu@cloudbasesolutions.com>
    (cherry picked from commit 811e30dfcab21bea7107351b2def2e85382fd296)

--- charms/ironic-conductor-k8s/tests/unit/test_charm.py
+++ charms/ironic-conductor-k8s/tests/unit/test_charm.py
@@ -16,9 +16,14 @@
 
 """Unit tests for the Ironic Conductor K8s charm."""
 
+import unittest.mock as mock
+
+import api_utils
 import charm
 import ops_sunbeam.test_utils as test_utils
+from keystoneauth1 import exceptions as ks_exc
 from ops.testing import (
+    ActionFailed,
     Harness,
 )
 
@@ -109,3 +114,156 @@ class TestIronicConductorOperatorCharm(test_utils.CharmTestCase):
         ]
         for f in config_files:
             self.check_file("ironic-conductor", f)
+
+    @mock.patch("keystoneclient.v3.Client")
+    @mock.patch("swiftclient.Connection")
+    @mock.patch("glanceclient.Client")
+    @mock.patch("keystoneauth1.session.Session")
+    @mock.patch("keystoneauth1.loading.get_plugin_loader")
+    def test_set_temp_url_secret_failures(
+        self,
+        mock_get_plugin_loader,
+        mock_session,
+        mock_glance_client,
+        mock_swift_client,
+        mock_keystone_client,
+    ):
+        """Test the set-temp-url-secret action failures."""
+        # Run the action - should fail since it's not the leader.
+        with self.assertRaises(ActionFailed) as ctx:
+            self.harness.run_action("set-temp-url-secret")
+
+        self.assertEqual(
+            "action must be run on the leader unit.", str(ctx.exception)
+        )
+
+        # Set the leader, rerun the action, but relations are not set.
+        test_utils.set_all_pebbles_ready(self.harness)
+        self.harness.set_leader()
+
+        with self.assertRaises(ActionFailed) as ctx:
+            self.harness.run_action("set-temp-url-secret")
+
+        self.assertIn(
+            "required relations are not yet available", str(ctx.exception)
+        )
+
+        # This adds all the default / common relations.
+        # Test the keystone session creation case.
+        mock_session.side_effect = Exception("to be expected.")
+        test_utils.add_all_relations(self.harness)
+
+        with self.assertRaises(ActionFailed) as ctx:
+            self.harness.run_action("set-temp-url-secret")
+
+        self.assertIn("failed to create keystone session", str(ctx.exception))
+        mock_get_plugin_loader.assert_called_once_with("v3password")
+        mock_loader = mock_get_plugin_loader.return_value
+        mock_loader.load_from_options.assert_called_once_with(
+            username="username",
+            password="user-password",
+            project_name="user-project",
+            auth_url="http://10.153.2.45:80/openstack-keystone",
+            project_domain_name="pdomain_-ame",
+            user_domain_name="udomain-name",
+        )
+        mock_session.assert_called_once_with(
+            auth=mock_loader.load_from_options.return_value,
+            verify=api_utils.SYSTEM_CA_BUNDLE,
+        )
+
+        # Swift not yet available.
+        mock_session.side_effect = None
+        mock_keystone_client.return_value.endpoints.find.side_effect = (
+            ks_exc.http.NotFound
+        )
+
+        with self.assertRaises(ActionFailed) as ctx:
+            self.harness.run_action("set-temp-url-secret")
+
+        self.assertIn("Swift not yet available.", str(ctx.exception))
+        mock_glance_client.assert_called_once_with(
+            session=mock_session.return_value,
+            version=2,
+        )
+        mock_swift_client.assert_called_once_with(
+            session=mock_session.return_value,
+            cacert=api_utils.SYSTEM_CA_BUNDLE,
+        )
+        mock_keystone_client.assert_called_once_with(
+            session=mock_session.return_value,
+        )
+
+        # Glance not yet available.
+        mock_keystone_client.return_value.endpoints.find.side_effect = [
+            mock.sentinel.swift_service,
+            ks_exc.http.NotFound,
+        ]
+
+        with self.assertRaises(ActionFailed) as ctx:
+            self.harness.run_action("set-temp-url-secret")
+
+        self.assertIn("Glance not yet available.", str(ctx.exception))
+
+        # Glance store does not have Swift storage backend.
+        mock_keystone_client.return_value.endpoints.find.side_effect = None
+
+        with self.assertRaises(ActionFailed) as ctx:
+            self.harness.run_action("set-temp-url-secret")
+
+        self.assertIn(
+            "Glance does not support Swift storage backend.",
+            str(ctx.exception),
+        )
+
+    @mock.patch("keystoneclient.v3.Client")
+    @mock.patch("swiftclient.Connection")
+    @mock.patch("glanceclient.Client")
+    @mock.patch("keystoneauth1.session.Session")
+    @mock.patch("keystoneauth1.loading.get_plugin_loader")
+    def test_set_temp_url_secret(
+        self,
+        mock_get_plugin_loader,
+        mock_session,
+        mock_glance_client,
+        mock_swift_client,
+        mock_keystone_client,
+    ):
+        """Test the set-temp-url-secret action."""
+        # Set the leader, add all the default / common relations.
+        test_utils.set_all_pebbles_ready(self.harness)
+        self.harness.set_leader()
+        test_utils.add_all_relations(self.harness)
+
+        mock_glance_client.return_value.images.get_stores_info.return_value = {
+            "stores": [
+                {"id": "swift"},
+            ],
+        }
+
+        # Create secret.
+        action_event = self.harness.run_action("set-temp-url-secret")
+
+        self.assertEqual(
+            "Temp URL secret set.", action_event.results.get("output")
+        )
+        secret = self.harness.charm.leader_get("temp_url_secret")
+        mock_swift_client.return_value.post_account.assert_called_once_with(
+            {"x-account-meta-temp-url-key": secret},
+        )
+
+        # Secret already exists.
+        mock_swift_client.return_value.post_account.reset_mock()
+        secret = self.harness.charm.leader_get("temp_url_secret")
+        mock_swift_client.return_value.get_account.return_value = [
+            {
+                "x-account-meta-temp-url-key": secret,
+            },
+        ]
+
+        action_event = self.harness.run_action("set-temp-url-secret")
+
+        self.assertEqual(
+            "Temp URL secret set.", action_event.results.get("output")
+        )
+        mock_swift_client.return_value.post_account.assert_not_called()
