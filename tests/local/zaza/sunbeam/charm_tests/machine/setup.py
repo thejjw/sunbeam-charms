8996a9f20cac537531124f24b6654b2174153d1e
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2025-10-08 16:09:37 +0530

    [WIP] Extend CI tests to include machine charms
    Add machine charms in tests/all-k8s
    Deploy machine charms as part of configure
    Integrate cross model relations
    Remove excluded tempest smoke tests
    
    Change-Id: I5e2d899c6bebbca4a9da9c0137ff65c7df076720
    Signed-off-by: Hemanth Nakkina <hemanth.nakkina@canonical.com>

---
+++ tests/local/zaza/sunbeam/charm_tests/machine/setup.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2025 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Deploy machine applications.
+
+Deploy machine applications and integrations using jubilant.
+"""
+
+import logging
+from pathlib import (
+    Path,
+)
+
+import jubilant
+import zaza.model
+
+MACHINE_MODEL = "controller"
+
+
+def replace_model_in_bundle(bundle: Path, words_to_replace: dict):
+    """Replace words in a file."""
+    content = bundle.read_text()
+    for old_word, new_word in words_to_replace.items():
+        logging.debug(f"Replacing {old_word} with {new_word}")
+        modified_content = content.replace(old_word, new_word)
+
+    bundle.write_text(modified_content)
+
+
+def deploy_machine_applications():
+    """Deploy Machine applications.
+
+    Deploy machine applications like hypervisor, microceph, cinder-volume.
+    Perform necessary cross model integrations.
+    Wait for the applications to be active.
+    """
+    k8s_model = zaza.model.get_juju_model()
+
+    logging.debug("Updating machine bundle")
+    bundle = "./tests/all-k8s/bundles/machines.yaml"
+    words_to_replace = {"K8S_MODEL": k8s_model}
+    replace_model_in_bundle(Path(bundle), words_to_replace)
+
+    logging.info(bundle)
+    juju = jubilant.Juju(model=MACHINE_MODEL)
+    juju.cli("deploy", str(bundle), "--map-machines=existing,0=0")
+
+    # Wait for machine applications to be in relevant status
+    juju.wait(
+        lambda status: jubilant.all_active(
+            status,
+            "microceph",
+            "cinder-microceph",
+            "hypervisor",
+            "sunbeam-machine",
+            "epa-orchestrator",
+        ),
+        timeout=1200,
+    )
+
+    # cinder-volume is in blocked as it needs to integrate with cinder-k8s
+    try:
+        juju.cli(
+            "integrate",
+            "--model",
+            k8s_model,
+            "cinder:storage-backend",
+            "admin/controller.storage-backend",
+            include_model=False,
+        )
+    except jubilant.CLIError as e:
+        if "already exists" not in e.stderr:
+            raise e
+
+    try:
+        juju.cli(
+            "integrate",
+            "--model",
+            k8s_model,
+            "gnocchi:ceph",
+            "admin/controller.ceph",
+            include_model=False,
+        )
+    except jubilant.CLIError as e:
+        if "already exists" not in e.stderr:
+            raise e
+
+    # Include manila integration once microceph changes are published in squid/stable
+    # try:
+    #     juju.cli(
+    #         "integrate",
+    #         "--model",
+    #         k8s_model,
+    #         "manila-cephfs:ceph-nfs",
+    #         "admin/controller.ceph",
+    #         include_model=False
+    #     )
+    # except jubilant.CLIError as e:
+    #     if "already exists" not in e.stderr:
+    #         raise e
+
+    juju.wait(
+        lambda status: jubilant.all_active(status, "cinder-volume"),
+        timeout=600,
+    )
+
+    juju_k8s = jubilant.Juju(model=k8s_model)
+    juju_k8s.wait(
+        lambda status: jubilant.all_active(
+            status, "cinder", "ceilometer", "gnocchi"
+        ),
+        timeout=1200,
+    )
