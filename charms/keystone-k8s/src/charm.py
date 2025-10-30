c9871b47b06bb35869ec1d6c6eb47f303743bf96
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2025-10-30 13:28:37 +0000

    Allow using Keystone from a separate region
    In multi-region environments, Keystone services will reside
    in a separate region.
    
    As such, we need to specify the Keystone region in the
    [keystone_authtoken] section, otherwise the API services
    (such as Nova) will expect to find the identity service in the
    same (secondary) region, failing to authenticate user tokens:
    
      nova.api.openstack keystoneauth1.exceptions.catalog.EndpointNotFound:
      internal endpoint for identity service in SecondRegion region not found
    
    We'll fetch the Keystone region from the identity relation data.
    
    Change-Id: I818e1d5964819cdf3c92585fbd9544a8d9ce10b6
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- charms/keystone-k8s/src/charm.py
+++ charms/keystone-k8s/src/charm.py
@@ -2195,6 +2195,7 @@ export OS_AUTH_VERSION=3
                 self.public_endpoint,
                 service_credentials,
                 self.admin_role,
+                self.model.config["region"],
             )
 
     def add_credentials_from_event(self, event):
@@ -2262,7 +2263,7 @@ export OS_AUTH_VERSION=3
             user_domain_id=service_domain.get("id"),
             project_domain_name=service_domain.get("name"),
             project_domain_id=service_domain.get("id"),
-            region=self.model.config["region"],  # XXX(wolsen) region matters?
+            region=self.model.config["region"],
             admin_role=self.admin_role,
         )
 
