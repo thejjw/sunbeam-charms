6bc252012e8627398c6840e3cf53affa96efb01c
Ahmad Hassan <ahmad.hassan@canonical.com>
2026-01-19 17:53:11 +0500

    Fix pep8 on CI
    Change-Id: I0bfe2447c65f281a1ccd20b32fe504db9ccfa113
    Signed-off-by: Ahmad Hassan <ahmad.hassan@canonical.com>

--- charms/tempest-k8s/src/utils/cleanup.py
+++ charms/tempest-k8s/src/utils/cleanup.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Utils for cleaning up tempest-related resources."""
+
 import argparse
 import os
 from collections.abc import (
--- charms/tempest-k8s/src/utils/types.py
+++ charms/tempest-k8s/src/utils/types.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Types for the tempest charm."""
+
 from enum import (
     Enum,
 )
