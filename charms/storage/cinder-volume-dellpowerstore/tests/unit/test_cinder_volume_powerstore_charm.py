1dbe965651871581c15e219e44f50e5e0d0ea305
happystacker <jeanpierre.roquesalane@dell.com>
2026-01-27 10:53:49 +0000

    storage/dellpowerstore: add new charm
    Change-Id: I804b5ef484981e74d911878448f83ffe7cd9eda7
    Signed-off-by: happystacker <jeanpierre.roquesalane@dell.com>

---
+++ charms/storage/cinder-volume-dellpowerstore/tests/unit/test_cinder_volume_powerstore_charm.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for Cinder Dell PowerStore operator charm class."""
+
+from unittest.mock import (
+    MagicMock,
+    Mock,
+    patch,
+)
+
+import charm
+import ops.testing
+import ops_sunbeam.test_utils as test_utils
+
+
+class _CinderVolumeDellPowerStoreOperatorCharm(
+    charm.CinderVolumeDellPowerStoreOperatorCharm
+):
+    """Charm wrapper for test usage."""
+
+    def __init__(self, framework):
+        self.seen_events = []
+        super().__init__(framework)
+        self._snap = Mock()
+
+    def _log_event(self, event):
+        self.seen_events.append(type(event).__name__)
+
+    def get_snap(self):
+        return self._snap
+
+
+def add_complete_cinder_volume_relation(harness: ops.testing.Harness) -> int:
+    """Add a complete cinder-volume relation to the charm."""
+    return harness.add_relation(
+        "cinder-volume",
+        "cinder-volume",
+        unit_data={
+            "snap-name": "cinder-volume",
+        },
+    )
+
+
+class TestCinderDellPowerStoreOperatorCharm(test_utils.CharmTestCase):
+    """Test cases for CinderDellPowerStoreOperatorCharm class."""
+
+    PATCHES = []
+
+    def setUp(self):
+        """Setup fixtures ready for testing."""
+        super().setUp(charm, self.PATCHES)
+        self.mock_event = MagicMock()
+        self.snap = Mock()
+        snap_patch = patch.object(
+            _CinderVolumePowerStoreOperatorCharm,
+            "_import_snap",
+            Mock(return_value=self.snap),
+        )
+        snap_patch.start()
+        self.harness = test_utils.get_harness(
+            _CinderVolumePowerStoreOperatorCharm,
+            container_calls=self.container_calls,
+        )
+        self.addCleanup(snap_patch.stop)
+        self.addCleanup(self.harness.cleanup)
+
+    def test_all_relations(self):
+        """Test charm in context of full set of relations."""
+        self.harness.begin_with_initial_hooks()
+        san_ip_secret = self.harness.add_user_secret(
+            {"san-ip": "10.20.30.40"}
+        )
+        san_login_secret = self.harness.add_user_secret(
+            {"san-login": "admin"}
+        )   
+        san_password_secret = self.harness.add_user_secret(
+            {"san-password": "dangerous"}
+        )
+        add_complete_cinder_volume_relation(self.harness)
+        self.harness.grant_secret(san_ip_secret, self.harness.charm.app)
+        self.harness.grant_secret(san_login_secret, self.harness.charm.app)
+        self.harness.grant_secret(san_password_secret, self.harness.charm.app)
+        self.harness.update_config(
+            {
+                "san-ip": san_ip_secret,
+                "san-login": san_login_secret,
+                "san-password": san_password_secret,
+                "storage-protocol": "fc",
+            }
+        )
+        self.harness.evaluate_status()
+        self.assertSetEqual(
+            self.harness.charm.get_mandatory_relations_not_ready(
+                self.mock_event
+            ),
+            set(),
+        )
