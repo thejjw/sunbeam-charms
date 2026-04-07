2e3ec8efd64ade700ee3ca7c984ee4f218ccbab4
Ahmad Hassan <ahmad.hassan@canonical.com>
2026-04-07 15:55:32 +0500

    feat: generate dellpowermax charm
    Change-Id: I44ac0121d87f52970f94a8eb84bde1b79a2ae0c9
    Signed-off-by: Ahmad Hassan <ahmad.hassan@canonical.com>

--- charms/storage/cinder-volume-dellpowermax/tests/unit/__init__.py
+++ charms/storage/cinder-volume-dellpowermax/tests/unit/__init__.py
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit testing module for Cinder Volume Dell PowerStore operator."""
+"""Unit testing module for Cinder Volume Dell PowerMax operator."""
