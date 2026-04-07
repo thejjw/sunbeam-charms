61a66c0d9b74ae8b5fcd043bf50f44c57a4119d5
Ahmad Hassan <ahmad.hassan@canonical.com>
2026-04-07 17:50:55 +0500

    feat: generate dellpowervault charm
    Change-Id: Ie4d10a51d7f7c8a95bb00a433a13fe0aff827fda
    Signed-off-by: Ahmad Hassan <ahmad.hassan@canonical.com>
    (cherry picked from commit 54a6d149bf3639325e1a3f80a7fc2a8af147feb8)

---
+++ charms/storage/cinder-volume-dellpowervault/src/charm.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+#
+# Copyright 2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Cinder dellpowervault Operator Charm.
+
+This charm provide Cinder <-> dellpowervault integration as part
+of an OpenStack deployment
+"""
+
+import logging
+import typing
+from enum import (
+    StrEnum,
+)
+
+import ops
+import ops_sunbeam.charm as charm
+import ops_sunbeam.storage as sunbeam_storage
+import ops_sunbeam.tracing as sunbeam_tracing
+
+logger = logging.getLogger(__name__)
+
+
+class Protocol(StrEnum):
+    """Enumeration of valid protocol types."""
+
+    FC = "fc"
+    ISCSI = "iscsi"
+
+
+@sunbeam_tracing.trace_sunbeam_charm
+class CinderVolumeDellpowervaultOperatorCharm(
+    charm.OSCinderVolumeDriverOperatorCharm
+):
+    """Cinder/Dellpowervault Operator charm."""
+
+    service_name = "cinder-volume-dellpowervault"
+
+    @property
+    def backend_key(self) -> str:
+        """Return the Cinder backend section key for Dell PowerVault."""
+        return "dellpowervault." + self.model.app.name
+
+    def _configuration_type_overrides(self) -> dict[str, typing.Any]:
+        overrides = super()._configuration_type_overrides()
+        overrides.update(
+            {
+                "protocol": typing.Annotated[
+                    typing.Literal["fc", "iscsi"], sunbeam_storage.Required
+                ],
+            }
+        )
+        return overrides
+
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(CinderVolumeDellpowervaultOperatorCharm)
