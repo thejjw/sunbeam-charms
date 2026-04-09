de0ae5b91e6ae2ba1e1eb3753ed979acfc5409e8
Ahmad Hassan <ahmad.hassan@canonical.com>
2026-04-09 13:13:56 +0500

    feat: generate ibmgpfs charm
    Change-Id: Id2f306f8c0e280677932a3a68312598cf58b5670
    Signed-off-by: Ahmad Hassan <ahmad.hassan@canonical.com>
    (cherry picked from commit fbe20a782d2c81d2eff870ac216e6307f2abc80b)

---
+++ charms/storage/cinder-volume-ibmgpfs/src/charm.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+#
+# Copyright 2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Cinder ibmgpfs Operator Charm.
+
+This charm provide Cinder <-> ibmgpfs integration as part
+of an OpenStack deployment
+"""
+
+import logging
+from enum import StrEnum
+import typing
+
+import ops
+import ops_sunbeam.charm as charm
+import ops_sunbeam.storage as sunbeam_storage
+import ops_sunbeam.tracing as sunbeam_tracing
+import pydantic
+
+logger = logging.getLogger(__name__)
+
+
+class ImagesShareMode(StrEnum):
+    """Enumeration of valid image share modes."""
+
+    COPY = "copy"
+    COPY_ON_WRITE = "copy_on_write"
+
+
+@sunbeam_tracing.trace_sunbeam_charm
+class CinderVolumeIbmgpfsOperatorCharm(charm.OSCinderVolumeDriverOperatorCharm):
+    """Cinder/Ibmgpfs Operator charm."""
+
+    service_name = "cinder-volume-ibmgpfs"
+
+    @property
+    def backend_key(self) -> str:
+        """Return the Cinder backend section key for IBM GPFS."""
+        return "ibmgpfs." + self.model.app.name
+
+    def _configuration_type_overrides(self) -> dict[str, typing.Any]:
+        overrides = super()._configuration_type_overrides()
+        overrides.pop("driver-ssl-cert", None)
+        overrides.update(
+            {
+                "gpfs-images-share-mode": ImagesShareMode | None,
+                "gpfs-user-password": typing.Annotated[
+                    str,
+                    pydantic.BeforeValidator(sunbeam_storage.secret_validator("gpfs-user-password")),
+                    sunbeam_storage.Required,
+                ],
+                "san-login": typing.Annotated[
+                    str,
+                    pydantic.BeforeValidator(sunbeam_storage.secret_validator("san-login")),
+                    sunbeam_storage.Required,
+                ],
+                "san-password": typing.Annotated[
+                    str,
+                    pydantic.BeforeValidator(sunbeam_storage.secret_validator("san-password")),
+                    sunbeam_storage.Required,
+                ],
+            }
+        )
+        return overrides
+
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(CinderVolumeIbmgpfsOperatorCharm)
